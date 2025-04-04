ea88e37c7df3817f5b1b25c88a69a97ad7ec074d
inmove <lisper.inmove@gmail.com>
2025-04-04 21:58:41 +0800

    map for array

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,10 +1,42 @@
 function Greet() {
+  const numbers = [1, 2, 3, 4, 5];
+  const userInfos = [
+    {
+      name: "inmove",
+      age: 18,
+    },
+    {
+      name: "andy",
+      age: 3,
+    },
+  ];
+
   return (
     <section>
       <h1>Hello</h1>
       <label htmlFor="Name">My Label</label>
       <input id="Name" type="text" placeholder="Enter your name" /> <br />
       <label> {2 + 2} </label>
+      {numbers.map((v) => (
+        <ul key={v}>
+          <li>{v}</li>
+        </ul>
+      ))}
+      {numbers.map((v) => {
+        return (
+          <ul key={`item-${v}`}>
+            <li>Value of v: {v}</li>
+          </ul>
+        );
+      })}
+      {userInfos.map(({ name, age }) => {
+        return (
+          <ul key={name}>
+            <li>{name}</li>
+            <li>{age}</li>
+          </ul>
+        );
+      })}
     </section>
   );
 }
