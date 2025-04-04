c4071b6cf9bc90691f1377b7f84b2a238a4c732e
inmove <lisper.inmove@gmail.com>
2025-04-04 21:40:04 +0800

    use brackets in jsx

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,5 +1,12 @@
 function Greet() {
-  return <section>Hello!</section>;
+  return (
+    <section>
+      <h1>Hello</h1>
+      <label htmlFor="Name">My Label</label>
+      <input id="Name" type="text" placeholder="Enter your name" /> <br />
+      <label> {2 + 2} </label>
+    </section>
+  );
 }
 
 export default Greet;
